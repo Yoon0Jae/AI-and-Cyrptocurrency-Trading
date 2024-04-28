dae21cbc5c61371afda42b36002fec4d6698070a
Yoon0Jae <167828582+Yoon0Jae@users.noreply.github.com>
2024-04-28 18:53:48 +0900

    Create orderbook_ETH.py

---
+++ orderbook_ETH.py
@@ -0,0 +1,50 @@
+import time
+import requests
+import pandas as pd
+import datetime
+
+while(1):
+
+
+
+    book = {}
+    response = requests.get ('https://api.bithumb.com/public/orderbook/ETH_KRW/?count=5')
+    book = response.json()
+
+
+
+    data = book['data']
+
+    bids = (pd.DataFrame(data['bids'])).apply(pd.to_numeric,errors='ignore')
+    bids.sort_values('price', ascending=False, inplace=True)
+    bids = bids.reset_index(); del bids['index']
+    bids['type'] = 0
+
+    asks = (pd.DataFrame(data['asks'])).apply(pd.to_numeric,errors='ignore')
+    asks.sort_values('price', ascending=True, inplace=True)
+    asks['type'] = 1
+
+    print (bids)
+    print ("\n")
+    print (asks)
+
+    df = bids._append(asks)
+
+    timestamp = datetime.datetime.now()
+    req_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+    df['quantity'] = df['quantity'].round(decimals=4)
+    df['timestamp'] = req_timestamp
+
+    #print (df)
+    #print ("\n")
+
+    df.to_csv("./2024-04-28-bithumb-orderbook-ETH.csv", index=False, header=False, mode = 'a')
+
+    #should_write_header = os.path.exists(fn)
+    #if should_write_header == False:
+    #    df.to_csv(fn, index=False, header=True, mode = 'a')
+    #else:
+    #    df.to_csv(fn, index=False, header=False, mode = 'a')
+
+    time.sleep(5.0)
